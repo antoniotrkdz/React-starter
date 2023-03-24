a6f3a49e044774a3c38ed0616c9a1037f455a0ed
antoniotrkdz <antoniotrkdz@gmail.com>
2023-03-24 20:03:43 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return <h1>Rick and Morty</h1>
+}
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(<App />)
